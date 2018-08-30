cb39278d62fa1e6c96068f45784ee07729cc6d5e
Nikita <nborisov256@gmail.com>
2018-08-30 12:49:07 +0300

    14 урок

---
+++ Main/js/script.js
@@ -0,0 +1,17 @@
+window.addEventListener('DOMContentLoaded', function() {
+	let tab = require('../parts/tab.js');
+	let modal = require('../parts/modal.js');
+	let scroll = require('../parts/scroll.js');
+	let form = require('../parts/form.js');
+	let slider = require('../parts/slider.js');
+	let calculator = require('../parts/calculator.js');
+	let timer = require('../parts/timer.js');
+
+	tab();
+	modal();
+	scroll();
+	form();
+	slider();
+	calculator();
+	timer();
+});
